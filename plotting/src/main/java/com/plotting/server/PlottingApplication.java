6733ade18032f9e4c954a593c768bec8ff2f12c4
syjdjr <syjdjr@naver.com>
2024-10-08 14:42:14 +0900

    ➕add/ 기초 세팅

---
+++ plotting/src/main/java/com/plotting/server/PlottingApplication.java
@@ -0,0 +1,13 @@
+package com.plotting.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlottingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlottingApplication.class, args);
+	}
+
+}
---
+++ plotting/src/test/java/com/plotting/server/PlottingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plotting.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlottingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
