5ccdb4a14e0cd9fdd69765477d26c0f20fed62de
hyemin <pphm12241224@gmail.com>
2024-10-09 11:10:41 +0900

    :sparkles: feat: #4 유저 엔티티 수정

---
+++ plotting/src/main/java/com/plotting/server/user/domain/UserType/LoginType.java
@@ -0,0 +1,15 @@
+package com.plotting.server.user.domain.UserType;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum LoginType {
+    NAVER("naver"),
+    GOOGLE("google"),
+    SELF("self"),
+    ;
+
+    private final String loginType;
+}
