2a2eb27926cbb79ec2db6834d0a872bc9b99a53e
Amepistheo <kellylhji@gmail.com>
2024-10-12 20:02:27 +0900

    :sparkles: feat: #16 Plogging 더미데이터 추가

--- src/main/java/com/plotting/server/plogging/repository/PloggingRepository.java
+++ src/main/java/com/plotting/server/plogging/repository/PloggingRepository.java
@@ -1,4 +1,9 @@
 package com.plotting.server.plogging.repository;
 
-public interface PloggingRepository {
+import com.plotting.server.plogging.domain.Plogging;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PloggingRepository extends JpaRepository<Plogging, Long> {
 }
