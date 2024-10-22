b3cc027849d949b0618efccf43a8a73118185fb8
syjdjr <syjdjr@naver.com>
2024-10-22 14:54:02 +0900

    :sparkles: feat: #18 플로깅 모임을 등록을 위한 DTO request 구현

--- src/main/java/com/plotting/server/plogging/dto/request/PloggingRequest.java
+++ src/main/java/com/plotting/server/plogging/dto/request/PloggingRequest.java
@@ -1,4 +1,35 @@
 package com.plotting.server.plogging.dto.request;
 
-public record PloggingRequest() {
+import com.plotting.server.plogging.domain.Plogging;
+import com.plotting.server.plogging.domain.type.PloggingType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record PloggingRequest(
+        PloggingType type,
+        Long maxPeople,
+        LocalDate recruitStartDate,
+        LocalDate recruitEndDate,
+        String title,
+        String content,
+        LocalDateTime startTime,
+        Long spendTime,
+        String startLocation,
+        String endLocation
+) {
+    public Plogging toEntity(Plogging plogging) {
+        return Plogging .builder()
+                .title(title)
+                .content(content)
+                .maxPeople(maxPeople)
+                .ploggingType(type)
+                .recruitStartDate(recruitStartDate)
+                .startTime(startTime)
+                .recruitEndDate(recruitEndDate)
+                .spendTime(spendTime)
+                .startLocation(startLocation)
+                .endLocation(endLocation)
+                .build();
+    }
 }
