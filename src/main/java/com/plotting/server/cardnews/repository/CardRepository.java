b223f927cb633c54716143629cc622ec9ab655d6
syjdjr <syjdjr@naver.com>
2024-11-06 14:51:16 +0900

    :sparkles: feat: #22 카드뉴스 Respository 구현 및 테이블 생성

--- src/main/java/com/plotting/server/cardnews/repository/CardRepository.java
+++ src/main/java/com/plotting/server/cardnews/repository/CardRepository.java
@@ -1,12 +1,11 @@
 package com.plotting.server.cardnews.repository;
 
-import com.plotting.server.cardnews.domain.Cardnews;
+import com.plotting.server.cardnews.domain.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CardnewsRepository extends JpaRepository<Cardnews, Long> {
-    List<Cardnews> findAll();
+public interface CardRepository extends JpaRepository<Card, Long> {
 }
