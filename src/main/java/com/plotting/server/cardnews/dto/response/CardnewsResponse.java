a610b3aae50b28c62341fac0bbb8c4311c7d2d53
syjdjr <syjdjr@naver.com>
2024-11-06 14:50:47 +0900

    :sparkles: feat: #22 카드뉴스 dto Response 작성

--- src/main/java/com/plotting/server/cardnews/dto/response/CardnewsResponse.java
+++ src/main/java/com/plotting/server/cardnews/dto/response/CardnewsResponse.java
@@ -1,4 +1,17 @@
 package com.plotting.server.cardnews.dto.response;
 
-public record CardnewsResponse() {
+import com.plotting.server.cardnews.domain.Cardnews;
+import lombok.Builder;
+
+@Builder
+public record CardnewsResponse(
+        Long id,
+        String cardnewsTitle
+) {
+    public static CardnewsResponse from(Cardnews cardnews) {
+        return CardnewsResponse.builder()
+                .id(cardnews.getId())
+                .cardnewsTitle(cardnews.getTitle())
+                .build();
+    }
 }
