696a1a8c1369f09e59830c684f56703690d3d154
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-11-18 04:04:32 +0900

    ✨ feat: plogging 즐겨찾기 제거

--- src/main/java/com/plotting/server/plogging/presentation/PloggingStarController.java
+++ src/main/java/com/plotting/server/plogging/presentation/PloggingStarController.java
@@ -2,16 +2,15 @@ package com.plotting.server.plogging.presentation;
 
 import com.plotting.server.global.dto.ResponseTemplate;
 import com.plotting.server.plogging.application.PloggingStarService;
+import com.plotting.server.plogging.dto.request.PloggingStarRequest;
 import com.plotting.server.plogging.dto.response.MyPloggingStarListResponse;
+import com.plotting.server.user.dto.request.UserStarRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -29,5 +28,15 @@ public class PloggingStarController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ResponseTemplate.from(response));
+    }
+
+    @Operation(summary = "플로깅 즐겨 찾기 해제", description = "내가 즐겨찾기 한 플로깅 해제")
+    @DeleteMapping("/remove")
+    public ResponseEntity<ResponseTemplate<?>> removePloggingStar(@RequestBody PloggingStarRequest request){
+        log.info("RemoveUserStarRequest: {}", request); // 요청 데이터 로그 출력
+        ploggingStarService.deleteUserStar(request.userId(), request.ploggingId());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseTemplate.EMPTY_RESPONSE);
     }
 }
