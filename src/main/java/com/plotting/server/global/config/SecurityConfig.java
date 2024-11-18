3ad2529eac0b6d7a15be926051f21f629b0aa1c1
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-11-18 22:02:39 +0900

    ♻️ refactor: 시큐리티 config 수정

--- src/main/java/com/plotting/server/global/config/SecurityConfig.java
+++ src/main/java/com/plotting/server/global/config/SecurityConfig.java
@@ -37,8 +37,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf ->csrf.disable())        // CSRF 보호 비활성화 (REST API를 위한 설정)
                 .authorizeHttpRequests(auth->auth    // 권한 설정
-                        .requestMatchers("/auth/**",
-                                "/swagger-ui/", "/v3/api-docs/", "/swagger-resources/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
