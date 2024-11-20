b3bd322d87147cf9a38352d001e331099598d91b
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-11-21 02:25:20 +0900

    ♻️ refactor: 커스텀 userDetails 추가

--- src/main/java/com/plotting/server/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/plotting/server/global/filter/JwtAuthenticationFilter.java
@@ -1,7 +1,10 @@
 package com.plotting.server.global.filter;
 
+import com.plotting.server.global.dto.JwtUserDetails;
+import com.plotting.server.global.exception.errorcode.ErrorCode;
 import com.plotting.server.global.util.JwtUtil;
 import com.plotting.server.user.application.UserDetailsServiceImpl;
+import com.plotting.server.user.exception.UserNotFoundException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,11 +12,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
+
+import static com.plotting.server.user.exception.errorcode.UserErrorCode.USER_NOT_FOUND;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -26,15 +32,25 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = resolveToken(request);   // request 헤더에서 토큰 추출
 
         if(token != null && jwtUtil.validateToken(token)) {
-            Long userId =jwtUtil.getIdFromToken(token);
-            UserDetails userDetails = userDetailsService.loadUserByUserId(userId);
-            log.info("Extracted userId from JWT: {}", userId);
+            try {
+                Long userId = jwtUtil.getIdFromToken(token);
+                JwtUserDetails userDetails = userDetailsService.loadUserByUserId(userId);
+                log.info("Extracted userId from JWT: {}", userId);
 
-            if (userDetails != null) {
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            }else{
-                log.error("Invalid userId extracted from token.");
+                if (userDetails != null) {
+                    UsernamePasswordAuthenticationToken authenticationToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails,
+                                    null,
+                                    Collections.singletonList(new SimpleGrantedAuthority(userDetails.role().name())));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                } else {
+                    log.error("Invalid userId extracted from token.");
+                    throw new UserNotFoundException(USER_NOT_FOUND);
+                }
+            }catch (Exception ex){
+                log.error("Authentication failed: ", ex.getMessage());
+                SecurityContextHolder.clearContext();
             }
         }
         chain.doFilter(request, response);
