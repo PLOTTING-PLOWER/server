d55985efffdc197d37a6b358936cc656d0a63be8
himi-i <pphm1224@naver.com>
2024-11-25 17:49:30 +0900

    :bug: fix : 로그인 토큰 연결

--- src/main/java/com/plotting/server/plogging/presentation/MyPloggingController.java
+++ src/main/java/com/plotting/server/plogging/presentation/MyPloggingController.java
@@ -130,9 +130,9 @@ public class MyPloggingController {
     @Operation(summary = "참여했던 플로깅 조회", description = "참여했던 플로깅을 조회합니다.")
     @GetMapping("/participated")
     public ResponseEntity<ResponseTemplate<?>> getMyPloggingParticipated(
-            @RequestParam Long userId) {
+            @AuthenticationPrincipal JwtUserDetails jwtUserDetails) {
 
-        List<MyPloggingParticipatedResponse> response = myPloggingHomeService.getParticipatedPloggings(userId);
+        List<MyPloggingParticipatedResponse> response = myPloggingHomeService.getParticipatedPloggings(jwtUserDetails.userId());
 
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -142,57 +142,26 @@ public class MyPloggingController {
 
     @Operation(summary = "예정된 플로깅 조회", description = "예정된 플로깅을 조회합니다.")
     @GetMapping("/scheduled")
-    public ResponseEntity<ResponseTemplate<?>> getMyPloggingScheduled(@RequestParam Long userId) {
-        try {
-            // 요청받은 userId 출력
-            System.out.println("scheduled ploggings for userId: " + userId);
+    public ResponseEntity<ResponseTemplate<?>> getMyPloggingScheduled(
+            @AuthenticationPrincipal JwtUserDetails jwtUserDetails) {
 
             // 서비스 호출을 통해 데이터를 조회
-            List<MyPloggingScheduledResponse> response = myPloggingHomeService.getScheduledPloggings(userId);
-
-            if (response.isEmpty()) {
-                // 조회된 데이터가 없을 때 출력
-                System.out.println("No scheduled ploggings found for userId: " + userId);
-            }
+            List<MyPloggingScheduledResponse> response = myPloggingHomeService.getScheduledPloggings(jwtUserDetails.userId());
 
             return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(ResponseTemplate.from(response));
-        } catch (Exception e) {
-            // 예외가 발생한 경우 출력
-            System.out.println("Error fetching ploggings for userId: " + userId);
-            e.printStackTrace(); // 예외 스택 트레이스 출력
-
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(ResponseTemplate.from("Error occurred"));
-        }
     }
 
     @Operation(summary = "나의 플로깅 횟수,시간 조회", description = "나의 플로깅 횟수,시간 조회합니다.")
     @GetMapping("/summary")
     public ResponseEntity<ResponseTemplate<?>> getPloggingSummary(
-            @RequestParam Long userId) {
-        try {
+            @AuthenticationPrincipal JwtUserDetails jwtUserDetails) {
 
-            MyPloggingSummaryResponse response = myPloggingHomeService.getPloggingSummary(userId);
-
-            if (response == null) {
-                // 조회된 데이터가 없을 때 출력
-                System.out.println("No Summary ploggings found for userId: " + userId);
-            }
+            MyPloggingSummaryResponse response = myPloggingHomeService.getPloggingSummary(jwtUserDetails.userId());
 
             return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(ResponseTemplate.from(response));
-        } catch (Exception e) {
-            // 예외가 발생한 경우 출력
-            System.out.println("Error fetching ploggings for userId: " + userId);
-            e.printStackTrace(); // 예외 스택 트레이스 출력
-
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(ResponseTemplate.from("Error occurred"));
-        }
     }
 }
