218c8d35332bf2aa3a73b3556fb8cf24c9d49931
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-11-28 02:53:48 +0900

    ✨ feat: fcm 알람 전송

--- src/main/java/com/plotting/server/alarm/repository/AlarmRepository.java
+++ src/main/java/com/plotting/server/alarm/repository/AlarmRepository.java
@@ -2,6 +2,11 @@ package com.plotting.server.alarm.repository;
 
 import com.plotting.server.alarm.domain.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+    List<Alarm> findAlarmByUserId(Long userId);
 }
