5588d55458793fecce84bffefa1b7b833931a3c1
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-11-29 22:56:21 +0900

    ♻️ refactor: dto 및 랭킹 null 수정

--- src/main/java/com/plotting/server/user/dto/response/MyUserStarResponse.java
+++ src/main/java/com/plotting/server/user/dto/response/MyUserStarResponse.java
@@ -9,7 +9,9 @@ public record MyUserStarResponse(
         Long userId,
         String profileImageUrl,
         String nickname,
-        String profileMessage
+        String profileMessage,
+        Boolean isProfilePublic
+
 ) {
     public static MyUserStarResponse from(UserStar userStar) {
         return MyUserStarResponse.builder()
@@ -17,6 +19,7 @@ public record MyUserStarResponse(
                 .profileImageUrl(userStar.getStarUser().getProfileImageUrl())
                 .nickname(userStar.getStarUser().getNickname())
                 .profileMessage(userStar.getStarUser().getProfileMessage())
+                .isProfilePublic(userStar.getStarUser().getIsProfilePublic())
                 .build();
     }
 }
