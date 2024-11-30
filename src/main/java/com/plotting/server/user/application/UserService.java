38a5920ec505fcf88562cdbdd9183ad8e9225ade
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-11-30 20:13:21 +0900

    ♻️ refactor: 네이버 아이디 로그인 수정

--- src/main/java/com/plotting/server/user/application/UserService.java
+++ src/main/java/com/plotting/server/user/application/UserService.java
@@ -19,6 +19,7 @@ import com.plotting.server.user.repository.UserRepository;
 import com.plotting.server.user.repository.UserStarRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
